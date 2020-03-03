fbb253bde09a17c0ca70392df4a80c92daa247e3
seungho Jeon <urijsh612@gmail.com>
2020-03-03 11:15:38 +0900

    주석추가: AppQueries의 로컬 query 수정

--- src/Components/App/AppQueries.ts
+++ src/Components/App/AppQueries.ts
@@ -4,10 +4,20 @@ import { gql } from "apollo-boost";
 // -> https://www.apollographql.com/docs/link/links/state/#client-directive
 // -> local storage에서 데이터 가져올 시 사용
 // -> 위의 쿼리 설명: local저장소에서 isLoggedIn 데이터 요청
+
+// export const IS_LOGGED_IN = gql`
+//   {
+//     auth {
+//       isLoggedIn @client
+//     }
+//   }
+// `;
+
+// 위의 gql문과 동일한 작용 (그냥 쿼리에 이름만 적음)
 export const IS_LOGGED_IN = gql`
-  {
-    auth {
-      isLoggedIn @client
+  query checkLogin {
+    auth @client {
+      isLoggedIn
     }
   }
 `;
