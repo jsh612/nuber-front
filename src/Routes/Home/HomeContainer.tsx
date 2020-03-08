da605df3f09326b49d5eda3998676b7f9d822a59
seungho Jeon <urijsh612@gmail.com>
2020-03-08 22:07:12 +0900

    #2.57,58 HomeScreen Getting Directions

--- src/Routes/Home/HomeContainer.tsx
+++ src/Routes/Home/HomeContainer.tsx
@@ -6,6 +6,7 @@ import { USER_PROFILE } from "../../sharedQueries.queries";
 import { userProfile } from "../../types/api";
 import useInput from "../../hooks/useInput";
 import { geoCode } from "../../mapHelpers";
+import { toast } from "react-toastify";
 
 interface IProps extends RouteComponentProps {
   google: typeof google;
@@ -22,9 +23,14 @@ const HomeContainer: React.FC<IProps> = () => {
   const [coords, setCoords] = useState<ICoords>({ lat: 0, lng: 0 });
   const [toCoords, setToCoords] = useState<ICoords>({ lat: 0, lng: 0 });
   const toAddressInput = useInput("");
+  const [distance, setDistance] = useState<string>("");
+  const [duration, setDuration] = useState<string>("");
+  const [price, setPrice] = useState<number>(0);
+  // 구긂맵 렌더와 마커 표시 관련
   const [itMap, setItMap] = useState<google.maps.Map>();
   const [userMarker, setUserMarker] = useState<google.maps.Marker>();
   const [toMarker, setToMarker] = useState<google.maps.Marker>();
+  const [directions, setDirections] = useState();
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -35,6 +41,7 @@ const HomeContainer: React.FC<IProps> = () => {
     const {
       coords: { latitude, longitude }
     } = position;
+    setCoords({ lat: latitude, lng: longitude });
     loadMap(latitude, longitude);
   };
 
@@ -70,8 +77,7 @@ const HomeContainer: React.FC<IProps> = () => {
         lng
       },
       disableDefaultUI: false,
-      zoom: 13,
-      minZoom: 8
+      zoom: 13
     };
     // maps.Map(엘리먼트, 구글맵 옵션)
     //  - Creates a new map inside of the given HTML container, which is typically a DIV element.
@@ -96,14 +102,6 @@ const HomeContainer: React.FC<IProps> = () => {
     setUserMarker(marker);
   };
 
-  useEffect(() => {
-    // 컴포넌트 렌더시 지도를 보여주기 위함
-    navigator.geolocation.getCurrentPosition(
-      handleGeoCurrentSucces,
-      handleGeoCurrentError
-    );
-  }, []);
-
   const onAddressSubmit = async () => {
     // 주소입력란에서 입력받은 주소를 이용하여 새로운 마커를 표시한다.
     const maps = google.maps;
@@ -112,9 +110,6 @@ const HomeContainer: React.FC<IProps> = () => {
     );
     if (result) {
       const { lat, lng, formatted_address: formatedAddress } = result;
-      setToCoords({ lat, lng });
-      toAddressInput.setValue(formatedAddress);
-
       if (toMarker) {
         // 기존에 체크한 목적지 마커가 있다면 없애기
         toMarker.setMap(null);
@@ -128,11 +123,81 @@ const HomeContainer: React.FC<IProps> = () => {
       };
       const toNewMarker = new maps.Marker(toMarkerOptions);
       setToMarker(toNewMarker);
+      const bounds = new google.maps.LatLngBounds(); // bounds(구역) 생성
+      bounds.extend({ lat, lng });
+      bounds.extend({ ...coords });
+
       if (itMap) {
         toNewMarker.setMap(itMap);
+        itMap.fitBounds(bounds); // 작성한 bounds(구역)에 맞춰 viewport 조정
       }
+      toAddressInput.setValue(formatedAddress);
+      setToCoords({ lat, lng });
+      createPath(lat, lng);
     }
   };
+
+  const handleRouteRequest = (directions: google.maps.DirectionsRenderer) => {
+    return (
+      result: google.maps.DirectionsResult,
+      status: google.maps.DirectionsStatus
+    ) => {
+      if (status === "OK") {
+        const { routes } = result;
+        const {
+          distance: { text: distance },
+          duration: { text: duration }
+        } = routes[0].legs[0];
+        setDistance(distance);
+        setDuration(duration);
+        directions.setDirections(result); // 경로렌더에 찾은 경로를 세팅
+        if (itMap) {
+          directions.setMap(itMap); // 찾은 경로 지도에 표시
+        }
+      } else {
+        toast.error("There is no route there, you have to ");
+      }
+    };
+  };
+
+  const createPath = (toLat: number, toLng: number) => {
+    const { lat, lng } = coords;
+    if (directions) {
+      directions.setMap(null);
+    }
+    const renderOptions: google.maps.DirectionsRendererOptions = {
+      polylineOptions: {
+        strokeColor: "#000"
+      },
+      suppressMarkers: true
+    };
+
+    // 1.google.maps.DirectionsRenderer --> 찾은 경로를 맵에 render
+    const newDirecions = new google.maps.DirectionsRenderer(renderOptions);
+    setDirections(newDirecions);
+
+    // 2. 경로 찾기
+    const directionsService = new google.maps.DirectionsService();
+    const to = new google.maps.LatLng(toLat, toLng);
+    const from = new google.maps.LatLng(lat, lng);
+    const directionsOptions: google.maps.DirectionsRequest = {
+      destination: to,
+      origin: from,
+      travelMode: google.maps.TravelMode.DRIVING // 한국에서는 DRIVING 안됨
+    };
+    directionsService.route(
+      directionsOptions,
+      handleRouteRequest(newDirecions)
+    );
+  };
+
+  useEffect(() => {
+    // 컴포넌트 렌더시 지도를 보여주기 위함
+    navigator.geolocation.getCurrentPosition(
+      handleGeoCurrentSucces,
+      handleGeoCurrentError
+    );
+  }, []);
 
   useEffect(() => {
     // 유저 디비아시의 위치값 추적 시키기
@@ -145,6 +210,7 @@ const HomeContainer: React.FC<IProps> = () => {
       watchOptions
     );
   }, [itMap, userMarker]);
+
   return (
     <HomePresenter
       isMenuOpen={isMenuOpen}
